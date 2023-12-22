6e88ff4263b2332da2276d367a078317fec003fb
Hardikgiri Gosai <hardik.gosai007@gmail.com>
2023-12-22 12:06:49 +0530

    mapping files added

--- Getri_UnitOfWork/Models/ProductMap.cs
+++ Getri_UnitOfWork/Models/ProductMap.cs
@@ -1,6 +1,17 @@
-﻿namespace Getri_UnitOfWork.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Getri_UnitOfWork.Models
 {
     public class ProductMap
     {
+        public ProductMap(EntityTypeBuilder<Product> entityBuilder)
+        {
+            entityBuilder.HasKey(t => t.ProductId);
+            entityBuilder.Property(t => t.ProductName).IsRequired();
+            entityBuilder.Property(t => t.ProductName).HasMaxLength(50);
+            entityBuilder.Property(t => t.ProductPrice).IsRequired();
+            entityBuilder.Property(t => t.ProductCategory).IsRequired();
+            //entityBuilder.HasOne(t => t.User).WithMany(t => t.Products).HasForeignKey(t => t.UserId);
+        }
     }
 }
--- Getri_UnitOfWork/Models/UserMap.cs
+++ Getri_UnitOfWork/Models/UserMap.cs
@@ -1,6 +1,20 @@
-﻿namespace Getri_UnitOfWork.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Getri_UnitOfWork.Models
 {
     public class UserMap
     {
+        public UserMap(EntityTypeBuilder<User> entityBuilder)
+        {
+            entityBuilder.HasKey(t => t.UserId);
+            entityBuilder.Property(t => t.UserName).IsRequired();
+            entityBuilder.Property(t => t.UserName).HasMaxLength(50);
+            entityBuilder.Property(t => t.UserEmail).IsRequired();            
+            entityBuilder.Property(t => t.UserContactNo).IsRequired();
+            entityBuilder.Property(t => t.UserContactNo).HasMaxLength(15);
+            entityBuilder.Property(t => t.UserAddress).IsRequired();
+            entityBuilder.Property(t => t.UserAddress).HasMaxLength(50);
+            //entityBuilder.HasMany(t => t.Products).WithOne(t => t.User).HasForeignKey(t => t.UserId);
+        }
     }
 }
